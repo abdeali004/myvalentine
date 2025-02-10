03094fc22098058a38129a43efa7525b0c652e13
abdeali004 <abdeali52.station@gmail.com>
2025-02-10 22:47:48 +0530

    Created a level of workflow for the website to make it live.

--- static/js/data.js
+++ static/js/data.js
@@ -1,12 +1,28 @@
 const data = {
     "1-0" : {
-        "gif-img" : "https://media.tenor.com/0cNM_9li440AAAAj/dudu-giving-flowers-bubu-flowers.gif",
+        "gif-img" : "https://media.tenor.com/qb-sI_XqLqAAAAAj/bubu-dudu-sseeyall.gif",
         "question" : "Hey! Will you be my valentine 👉👈??",
-        "movable-no" : false
+        "movable-button" : false
     },
     "1-1" : {
         "gif-img" : "https://media.tenor.com/M-3rzbe03eAAAAAj/dudu-wearing-glasses-standing.gif",
         "question" : "Think again! I am one of my kind 😎.",
-        "movable-no" : false
+        "movable-button" : false
+    },
+    "1-2" : {
+        "gif-img" : "https://media.tenor.com/mX64AbW856EAAAAj/dudu-dudu-angry.gif",
+        "question" : "This is your last chance to reconsider... Will you be my Valentine? 💖",
+        "movable-button" : true
+    },
+    "1-3" : {
+        "gif-img" : "https://media.tenor.com/sWXhCC4A2woAAAAj/bubu-bubu-dudu.gif",
+        "question" : "This is your last chance to reconsider... Will you be my Valentine? 💖",
+        "movable-button" : false
+    },
+    "2-1" : {
+        "gif-img" : "https://media.tenor.com/0cNM_9li440AAAAj/dudu-giving-flowers-bubu-flowers.gif",
+        "question" : "Hurrahhhh!!! you will be my Valentine? 💖",
+        "movable-button" : false,
+        "mirror-page" : true
     }
 }
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,50 @@
+let no_number = 0;
+let yes_number = 1;
+
 document.addEventListener('DOMContentLoaded', function() {
-    parseData("1-0");
-    const button = document.getElementById('movable-button');
+    parseData("1");
+    
+});
+
+function navigate(page) {
+    window.location.href = page;
+}
+
+function launchMirrorPage() {
+    let current_no_button = document.getElementById('normal-button-no') == null ? document.getElementById('movable-button-no') : document.getElementById('normal-button-no');
+    current_no_button.style.display = "none";
+    document.getElementById("normal-button-yes").innerText = "Special Room...";
+    document.getElementById("normal-button-yes").onclick = function() {
+        navigate("mirror");
+    };
+}
+
+function parseData(input) {
+    let number = input == "1" ? no_number++ : yes_number++;
+    let current_data = data[input + "-" + number];
+    let mirror_page = current_data.hasOwnProperty("mirror-page") ? current_data["mirror-page"] : false;
+    if (mirror_page == true){
+        launchMirrorPage();
+    }
+    document.getElementById('gif-img').src = current_data["gif-img"];
+    document.getElementById('question').innerText = current_data["question"];
+    // Get the hidden input field by its ID
+    if (current_data["movable-button"] == true) {
+        document.getElementById('normal-button-no').id = "movable-button-no";
+        enableMovableButtonNo();
+    }
+}
+
+function nextPageNo() {
+    parseData("1");
+}
+
+function nextPageYes() {
+    parseData("2");
+}
+
+function enableMovableButtonNo() {
+    const button = document.getElementById('movable-button-no');
 
     button.addEventListener('click', function() {
         // Generate random positions within the viewport
@@ -21,33 +65,4 @@ document.addEventListener('DOMContentLoaded', function() {
         button.style.top = `${randomTop}px`;
         button.style.left = `${randomLeft}px`;
     });
-});
-
-function navigate(page) {
-    window.location.href = page;
-}
-
-function parseData(input) {
-    let current_data = data[input];
-    document.getElementById('gif-img').src = current_data["gif-img"];
-    document.getElementById('question').innerText = current_data["question"];
-    // Get the hidden input field by its ID
-    document.getElementById('question-number').value = nextPageNumber(input);
-}
-
-function nextPageNumber(input) {
-// Split the string by the hyphen
-let parts = input.split('-');
-
-// Increment the second part (convert it to a number, increment, and convert back to a string)
-parts[1] = (parseInt(parts[1]) + 1).toString();
-
-// Join the parts back into the original format
-let newStr = parts.join('-');
-
-return newStr;
-}
-
-function nextPage() {
-    parseData(document.getElementById('question-number').value);
 }
