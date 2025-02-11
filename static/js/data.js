bc5d45c302b76c9b70cccfa2a132eef99efeb82f
abdeali004 <abdeali52.station@gmail.com>
2025-02-11 21:16:12 +0530

    Removed online gifs dependencies and added in static/gifs folder. Also, improved the view for mirror page.

--- static/js/data.js
+++ static/js/data.js
@@ -1,26 +1,26 @@
 const data = {
     "1-0" : {
-        "gif-img" : "https://media.tenor.com/qb-sI_XqLqAAAAAj/bubu-dudu-sseeyall.gif",
+        "gif-img" : "static/gifs/1-0.gif",
         "question" : "Hey! Will you be my valentine 👉👈??",
         "movable-button" : false
     },
     "1-1" : {
-        "gif-img" : "https://media.tenor.com/M-3rzbe03eAAAAAj/dudu-wearing-glasses-standing.gif",
+        "gif-img" : "static/gifs/1-1.gif",
         "question" : "Think again! I am one of my kind 😎.",
         "movable-button" : false
     },
     "1-2" : {
-        "gif-img" : "https://media.tenor.com/mX64AbW856EAAAAj/dudu-dudu-angry.gif",
+        "gif-img" : "static/gifs/1-2.gif",
         "question" : "This is your last chance to reconsider... Will you be my Valentine? 💖",
         "movable-button" : true
     },
     "1-3" : {
-        "gif-img" : "https://media.tenor.com/sWXhCC4A2woAAAAj/bubu-bubu-dudu.gif",
+        "gif-img" : "static/gifs/1-3.gif",
         "question" : "This is your last chance to reconsider... Will you be my Valentine? 💖",
         "movable-button" : false
     },
     "2-1" : {
-        "gif-img" : "https://media.tenor.com/0cNM_9li440AAAAj/dudu-giving-flowers-bubu-flowers.gif",
+        "gif-img" : "static/gifs/2-1.gif",
         "question" : "Hurrahhhh!!! you will be my Valentine? 💖",
         "movable-button" : false,
         "mirror-page" : true
